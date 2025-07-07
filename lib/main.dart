fef57b3c8617ba74bc0dfccbf78cf479addba567
Yogaone <yogawanadityapratama@gmail.com>
2025-07-07 10:14:43 +0700

    Belajar Flutter #2

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:app/screens/hello_world_screen.dart';
 import 'package:app/screens/image_screen.dart';
 import 'package:app/screens/row_column_screen.dart';
 import 'package:app/screens/style_text_and_icon_screen.dart';
+import 'package:app/screens/tab_bar_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Reiteration Flutter',
-      initialRoute: '/image_screen',
+      initialRoute: '/tab_bar',
       routes: {
         '/hello_world': (context) => const HelloWorldScreen(),
         '/style_text_and_icon': (context) => const StyleTextAndIconScreen(),
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
         '/row_column': (context) => const RowColumnScreen(),
         '/card_and_parsing_data': (context) => const CardAndParshingDataScreen(),
         '/bottom_nav_bar': (context) => const BottomNavBarScreen(),
+        '/tab_bar': (context) => const TabBarScreen(),
         '/image_screen': (context) => const ImageScreen(),
       },
     );
---
+++ lib/screens/tab_bar_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class TabBarScreen extends StatelessWidget {
+  const TabBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tab Bar'),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
+              Tab(icon: Icon(Icons.person), text: 'Profile'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Ini Halaman Home')),
+            Center(child: Text('Ini Halaman Search')),
+            Center(child: Text('Ini Halaman Profile')),
+          ],
+        ),
+      ),
+    );
+  }
+}
